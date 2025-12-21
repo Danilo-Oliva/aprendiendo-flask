649b8feb61c470389cd0fa668aeb500ecd29fb20
Icaro <danilo.oliva153@gmail.com>
2025-12-21 12:34:16 -0300

    ✨ [FEATURE] Diccionario 'Context' para el render
    Creé un diccionario que contiene todos los parametros necesarios para renderizar el template, para desempaquetarlo simplemente hay que colocar dos astericos antes del nombre del diccionario y sin el igual

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-
+items = ["ITEM 1", "ITEM 2", "ITEM 3", "ITEM 4"]
 @app.route("/index")
 def index():
     user_ip_information = request.remote_addr
@@ -17,6 +17,12 @@ def index():
 @app.route("/show_information_address")
 def show_information():
     user_ip = request.cookies.get("user_ip_information")
-    return render_template("ip_information.html", user_ip= user_ip)
+    #creo el diccionario que contendrá todos los parametros del render_template
+    context = {
+        "user_ip": user_ip,
+        "items": items
+    }
+    
+    return render_template("ip_information.html", **context)
 
 app.run(host="0.0.0.0", port=81, debug=True)
