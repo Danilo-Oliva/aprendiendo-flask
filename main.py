c73cb8e8b00d8db82594b7aa72f0d96b15c31b4e
Icaro <danilo.oliva153@gmail.com>
2025-12-21 21:09:28 -0300

    ✨ [FEATURE] Implemento de cookies y redirección
    Modifiqué la lógica para que no muestre la IP directamente. Ahora la ruta /index captura la IP, la guarda en una cookie y redirige a /show_information_address, donde finalmente se lee la cookie y se muestra el mensaje

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    user_information = request.remote_addr
-    return f"Hola como estas, tu dirección IP es la siguiente: {user_information}"
-
+@app.route("/index")
+def index():
+    user_ip_information = request.remote_addr
+    
+    #La persona que entre al index va a ser automaticamente dirigido hacia la ruta que le digamos al redirect
+    response = make_response(redirect("/show_information_address"))
+    #con esta response voy a crear una cookie
+    response.set_cookie("user_ip_information", user_ip_information)
+    
+    return response
+    
+@app.route("/show_information_address")
+def show_information():
+    user_ip = request.cookies.get("user_ip_information")
+    return f"Hola, tu dirección IP es: {user_ip}"
 
 app.run(host="0.0.0.0", port=81, debug=True)
