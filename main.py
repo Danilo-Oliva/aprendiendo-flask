298f0a82dc5dee481ce809f89ac0e6c93aa14bcf
Icaro <danilo.oliva153@gmail.com>
2025-12-24 18:43:46 -0300

    ✨ [UPDATE] Items
    La lista de items la cambié por algunos items reales en vez de item 1, item 2, etc

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-items = ["ITEM 1", "ITEM 2", "ITEM 3", "ITEM 4"]
+items = ["Arroz", "Huevos", "Café", "Leche"]
+
+
 @app.route("/index")
 def index():
     user_ip_information = request.remote_addr
-    
-    #La persona que entre al index va a ser automaticamente dirigido hacia la ruta que le digamos al redirect
+
+    # La persona que entre al index va a ser automaticamente dirigido hacia la ruta que le digamos al redirect
     response = make_response(redirect("/show_information_address"))
-    #con esta response voy a crear una cookie
+    # con esta response voy a crear una cookie
     response.set_cookie("user_ip_information", user_ip_information)
-    
+
     return response
-    
+
+
 @app.route("/show_information_address")
 def show_information():
     user_ip = request.cookies.get("user_ip_information")
-    #creo el diccionario que contendrá todos los parametros del render_template
-    context = {
-        "user_ip": user_ip,
-        "items": items
-    }
-    
+    # creo el diccionario que contendrá todos los parametros del render_template
+    context = {"user_ip": user_ip, "items": items}
+
     return render_template("ip_information.html", **context)
+
 
 app.run(host="0.0.0.0", port=81, debug=True)
