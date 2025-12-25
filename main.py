aa537439cef18b889ddfaf9b6b0c53bfe9ade7df
Icaro <danilo.oliva153@gmail.com>
2025-12-25 16:07:27 -0300

    ✨ [FEATURE] Bootstrap
    Importé el Flask_bootstrap y creé un objeto de la clase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstap = Bootstrap(app)
+
 
 items = ["Arroz", "Huevos", "Café", "Leche"]
 
