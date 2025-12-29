f4a7930c60a866ebdcf0dc72a7076b75f1f78282
Icaro <danilo.oliva153@gmail.com>
2025-12-28 23:44:24 -0300

    ♻️ [REFACTOR] Validaciones
    añadí los metodos post y get dentro del views y despues moví las validaciones

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,21 @@
 from . import auth # . es directorio actual
 from app.forms import LoginForm
-from flask import render_template
+from flask import render_template, flash, session, redirect, url_for
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 #acá renderizamos el formulario de login
 def login():
+  login_form = LoginForm()
+  
   context = {
-    "login_form":LoginForm()
+    "login_form":login_form
   }
+  
+  # validamos
+  if login_form.validate_on_submit():
+      username = login_form.username.data
+      session["username"] = username
+      flash("Nombre de usuario registrado correctamente")
+      return redirect(url_for("index", _external=True))
+
   return render_template("login.html", **context)
